69bcd7cb18302fe94e84907225c779fb8f1dbbff
vitalijs <vitalijs.shakels@gmail.com>
2017-11-28 23:00:45 +0200

    2nd recipe name and desc change

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -4,7 +4,7 @@ import { EventEmitter } from "@angular/core";
 export class RecipeService {
   private recipes : Recipe[] = [
     new Recipe('Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
-    new Recipe('Test Recipe2', 'This is simply a test2', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+    new Recipe('Another Test Recipe', 'This is simply another test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
   ];
   recipeSelected = new EventEmitter<Recipe>()
 
