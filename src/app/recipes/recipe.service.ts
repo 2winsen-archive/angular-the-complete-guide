0b693e82d954b51f7c444ed6151a835fa0692c5a
vitalijs <vitalijs.shakels@gmail.com>
2017-11-28 23:39:43 +0200

    recipe with ingredients

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,10 +1,27 @@
 import { Recipe } from "./recipe.model";
 import { EventEmitter } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
 
 export class RecipeService {
   private recipes : Recipe[] = [
-    new Recipe('Test Recipe', 'This is simply a test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg'),
-    new Recipe('Another Test Recipe', 'This is simply another test', 'https://upload.wikimedia.org/wikipedia/commons/1/15/Recipe_logo.jpeg')
+    new Recipe(
+      'Tasty Schnitzel', 
+      'A super-tasty schnitze - just awesome!', 
+      'https://upload.wikimedia.org/wikipedia/commons/0/0c/Milanesa.jpg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('Lemon', 1)
+      ]
+    ),
+    new Recipe(
+      'Big Fat Burger', 
+      'What else you need to say', 
+      'https://upload.wikimedia.org/wikipedia/commons/thumb/8/8f/NYC-Diner-Bacon-Cheeseburger.jpg/1024px-NYC-Diner-Bacon-Cheeseburger.jpg',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('Meat', 1)
+      ]
+    )
   ];
   recipeSelected = new EventEmitter<Recipe>()
 
