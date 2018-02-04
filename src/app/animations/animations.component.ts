e923975df38fa5d97902759b60adc4152c9935cf
2winsen <vitalijs.shakels@gmail.com>
2018-02-04 23:59:38 +0200

    add and remove item animation

--- src/app/animations/animations.component.ts
+++ src/app/animations/animations.component.ts
@@ -15,13 +15,54 @@ import { Component } from '@angular/core';
         'background-color': 'blue',
         transform: 'translateX(100px)'
       })),
+      transition('normal <=> highlighted', animate(300))
+    ]),
+    trigger('wildState', [
+      state('normal', style({
+        'background-color': 'red',
+        transform: 'translateX(0) scale(1)'
+      })),
+      state('highlighted', style({
+        'background-color': 'blue',
+        transform: 'translateX(100px) scale(1)'
+      })),
+      state('shrunken', style({
+        'background-color': 'green',
+        transform: 'translateX(0) scale(0.5)'
+      })),
       transition('normal => highlighted', animate(300)),
-      transition('highlighted => normal', animate(800))
+      transition('highlighted => normal', animate(800)),
+      transition('shrunken <=> *', [
+        animate(1000, style({
+          transform: 'translateX(0) scaleX(2) scaleY(0.5)'
+        })),
+        animate(500)
+      ])
+    ]),
+    trigger('list1', [
+      state('in', style({
+        opacity: 1,
+        transform: 'translateX(0)'
+      })),
+      transition('void => *', [
+        style({
+          opacity: 0,
+          transform: 'translateX(-100px)'
+        }),
+        animate(300)
+      ]),
+      transition('* => void', [
+        animate(300, style({
+          opacity: 0,
+          transform: 'translateX(100px)'
+        }))
+      ])
     ])
   ]
 })
 export class AnimationsComponent {
   state: 'normal' | 'highlighted' = 'normal';
+  wildState: 'normal' | 'highlighted' | 'shrunken' = 'normal';
   list = ['Milk', 'Sugar', 'Bread'];
 
   onAdd(item) {
@@ -34,5 +75,10 @@ export class AnimationsComponent {
 
   onAnimate() {
     this.state = this.state === 'normal' ? 'highlighted' : 'normal';
+    this.wildState = this.wildState === 'normal' ? 'highlighted' : 'normal';
+  }
+
+  onShrink() {
+    this.wildState = 'shrunken';
   }
 }
